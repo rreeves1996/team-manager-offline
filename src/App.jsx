12e20c049b69f074489ac14d7c1c5a76c5a98096
Ryan Reeves <reeves0ryan@gmail.com>
2022-09-24 16:29:08 -0700

    Initial filestructure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+        <Home />
+      <Footer />
+    </>
+  );
+}
+
