61d0df29acab13473a989abf2aef2588ee0b5b63
Ryan Reeves <reeves0ryan@gmail.com>
2022-09-30 23:08:00 -0700

    Started employee cards

--- src/components/cards/EmpCard.jsx
+++ src/components/cards/EmpCard.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import "../../assets/style/empcard.css"
 
 export default function EmpCard(props) {
   return (
     <>
         <div className="emp-card">
+          <div className="emp-card-header">
             <h6 className="emp-name"><strong>{props.name}</strong></h6>
             <h6 className="emp-role">{props.lead ? "Team Lead" : props.role}</h6>
-            <div className="divider"></div>
-            <div className="emp-contact-info">
-                <p><strong>Phone Number:</strong> {props.number}</p>
-                <p><strong>Email:</strong> {props.email}</p>
-                <p><strong>Time Zone:</strong> UTC -{props.timeZone}:00</p>
-            </div>
+          </div>
+
+          <div className="emp-contact-info">
+              <p><strong>Phone #:</strong> {props.number}</p>
+              <p><strong>Email:</strong> {props.email}</p>
+              <p><strong>Time:</strong> UTC -{props.timeZone}:00</p>
+          </div>
         </div>
     </>
   );
